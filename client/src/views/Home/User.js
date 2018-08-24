6f94248d37bc0494346eba59964e7e921a770668
jose alves sousa neto <a.sousajose@gmail.com>
2018-08-24 00:34:20 -0400

    Cadastro de Usuarios

--- client/src/views/Home/User.js
+++ client/src/views/Home/User.js
@@ -1,23 +1,26 @@
 import React from 'react'
 import { Image, Table } from 'semantic-ui-react'
 import Proptypes from 'prop-types'
+import moment from 'moment'
 
 const User = ({ user }) => (
   <Table.Row>
     <Table.Cell>
       <div>
-        <Image avatar src={user.file.url} width='50' alt={user.fir} default='' />
+        <Image
+          avatar
+          src={user.file.url}
+          width='50'
+          alt={user.fir}
+          default=''
+        />
         <span>{`${user.first_name} ${user.last_name}`}</span>
       </div>
     </Table.Cell>
     <Table.Cell collapsing textAlign='right'>
-      {user.created_at}
+      {moment(user.created_at).format('d MMM, YYYY, HH:mm')}
     </Table.Cell>
   </Table.Row>
 )
-
-User.prototype = {
-  user: Proptypes
-}
 
 export default User
